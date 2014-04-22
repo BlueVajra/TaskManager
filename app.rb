1c2f64afe010f1a09c11758fde0036d8553c971c
BlueVajra <cory.leistikow@gmail.com>
2014-04-21 22:13:24 -0600

    removed comments in app

--- app.rb
+++ app.rb
@@ -6,35 +6,26 @@ class App < Sinatra::Application
 
 
   get '/' do
-    #Project.new.tasks
-    tasks = Task.filter(:project_id=>current_project).all
+    tasks = Task.filter(:project_id => current_project).all
     current_project
-    #tasks = @current_project.tasks
     erb :index, locals: {
       tasks: tasks,
-      #tasks:  @current_project.tasks,
       projects: Project.all
     }
   end
 
   post '/task/new' do
-    #puts "CURRENT PROJECT: #{current_project_name}"
-    Task.create(:project_id => current_project, :task=>params[:add_item])
+    Task.create(:project_id => current_project, :task => params[:add_item])
     redirect '/'
   end
 
   delete '/delete/:id' do
-    #tasks_repo.delete(params[:id])
     Task[params[:id]].delete
     redirect '/'
   end
 
   put '/complete/:id' do
-    #tasks_repo.complete(params[:id])
-    #@db[id: id][:completed]
-    completed?(params[:id]) ? Task[id: params[:id]].update(completed:false) : Task[id: params[:id]].update(completed:true)
-
-    #Task[params[:id]].update()
+    completed?(params[:id]) ? Task[id: params[:id]].update(completed: false) : Task[id: params[:id]].update(completed: true)
     redirect '/'
   end
 
@@ -44,8 +35,7 @@ class App < Sinatra::Application
   end
 
   get '/project/:id/current' do
-    #@current_project = Project[params[:id]]
-      make_current(params[:id])
+    make_current(params[:id])
     redirect '/'
   end
 
@@ -53,19 +43,10 @@ class App < Sinatra::Application
     def current_project
       if projects_repo.first.nil?
         projects_repo.add("Default Project")
-        #@current_project = Project[:project_name => "Default Project"][:project_id].values
         session[:current_project] = projects_repo.first[:project_id]
       else
-        #@current_project ||= Project.first
         session[:current_project] ||= projects_repo.first[:project_id]
       end
-      #if Project.first.nil?
-      #  Project.create(:project_name => "Default Project")
-      #end
-      #  @current_project ||= Project.first
-
-
-
     end
 
     def current_project_name
